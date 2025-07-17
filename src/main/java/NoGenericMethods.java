4140b328c66e38f3daef18881a483ba5dc95527c
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-17 12:18:58 +0200

    feat: create NoGenericsMethod class

--- src/main/java/NoGenericMethods.java
+++ src/main/java/NoGenericMethods.java
@@ -1,2 +1,36 @@
 public class NoGenericMethods {
+    private String firstArgument;
+    private String secondArgument;
+    private String thirdArgument;
+
+    public NoGenericMethods(String firstArgument, String secondArgument, String thirdArgument) {
+        this.firstArgument = firstArgument;
+        this.secondArgument = secondArgument;
+        this.thirdArgument = thirdArgument;
+    }
+
+
+    public String getFirstArgument() {
+        return firstArgument;
+    }
+
+    public void setFirstArgument(String firstArgument) {
+        this.firstArgument = firstArgument;
+    }
+
+    public String getSecondArgument() {
+        return secondArgument;
+    }
+
+    public void setSecondArgument(String secondArgument) {
+        this.secondArgument = secondArgument;
+    }
+
+    public String getThirdArgument() {
+        return thirdArgument;
+    }
+
+    public void setThirdArgument(String thirdArgument) {
+        this.thirdArgument = thirdArgument;
+    }
 }
