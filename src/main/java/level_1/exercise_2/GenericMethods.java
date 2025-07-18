f169aab4477a8a6c5bdb0590001dfa1678539ee3
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-18 14:23:51 +0200

    feat: call the generic method with different types of args

--- src/main/java/level_1/exercise_2/GenericMethods.java
+++ src/main/java/level_1/exercise_2/GenericMethods.java
@@ -3,6 +3,6 @@ package level_1.exercise_2;
 public class GenericMethods {
 
     public static <T> void printArguments(T firstArgument, T secondArgument, T thirdArgument) {
-        System.out.printf("%s%n%s%n%s%n", firstArgument, secondArgument, thirdArgument);
+        System.out.printf("%s%n%s%n%s%n%n", firstArgument, secondArgument, thirdArgument);
     }
 }
--- src/main/java/level_1/exercise_2/Main.java
+++ src/main/java/level_1/exercise_2/Main.java
@@ -3,10 +3,15 @@ package level_1.exercise_2;
 public class Main {
 
     public static void main(String[] args) {
-        Person person = new Person("Sophia", "Garcia", 45);
-        String task = "1.6-Generics";
-        int year = 2025;
+        Person personExample = new Person("Sophia", "Garcia", 45);
+        String stringExample = "1.6-Generics";
+        int intExample = 2025;
+        double doubleExample = 4.5;
+        char charExample = 'c';
 
-        GenericMethods.printArguments(person, task, year);
+
+        GenericMethods.printArguments(personExample, stringExample, intExample);
+        GenericMethods.printArguments(stringExample, doubleExample, personExample);
+        GenericMethods.printArguments(intExample, charExample, doubleExample);
     }
 }
