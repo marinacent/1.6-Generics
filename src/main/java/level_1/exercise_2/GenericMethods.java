234e22c5006f07c3bcfb0ac4b48cff1786321886
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-21 12:39:15 +0200

    fix: specify different genertic types in method signature

--- src/main/java/level_1/exercise_2/GenericMethods.java
+++ src/main/java/level_1/exercise_2/GenericMethods.java
@@ -2,7 +2,7 @@ package level_1.exercise_2;
 
 public class GenericMethods {
 
-    public static <T> void printArguments(T firstArgument, T secondArgument, T thirdArgument) {
+    public static <T1, T2, T3> void printArguments(T1 firstArgument, T2 secondArgument, T3 thirdArgument) {
         System.out.printf("%s%n%s%n%s%n%n", firstArgument, secondArgument, thirdArgument);
     }
 }
--- src/main/java/level_2/exercise_1/GenericStringMethod.java
+++ src/main/java/level_2/exercise_1/GenericStringMethod.java
@@ -2,7 +2,8 @@ package level_2.exercise_1;
 
 public class GenericStringMethod {
 
-    public static <T> void printArguments(T firstArgument, T secondArgument, String thirdArgument) {
+    public static <T1, T2> void printArguments(T1 firstArgument, T2 secondArgument,
+                                               String thirdArgument) {
         System.out.printf("%s%n%s%n%s%n%n", firstArgument, secondArgument, thirdArgument);
     }
 }
