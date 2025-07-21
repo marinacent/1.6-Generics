bdcc2fbbdc0114639300e8c9909071e6814134a9
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-21 09:54:45 +0200

    feat: add not null annotation to list printing method

--- src/main/java/level_2/exercise_2/GenericListMethod.java
+++ src/main/java/level_2/exercise_2/GenericListMethod.java
@@ -1,10 +1,12 @@
 package level_2.exercise_2;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class GenericListMethod {
 
-    public static <T> void printArguments(List<T> argList) {
+    public static <T> void printArguments(@NotNull List<T> argList) {
         for (T arg : argList) {
             System.out.println(arg);
         }
